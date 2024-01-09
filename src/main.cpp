b61d681cd15945af9d0ac817551a722f13f1d84f
simsax <simsax98@gmail.com>
2024-01-09 17:44:27 +0100

    fly camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <SDL_keycode.h>
+#include <SDL_mouse.h>
+#include <SDL_stdinc.h>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -36,6 +39,7 @@ struct Vertex {
     glm::vec2 texture;
 };
 
+
 int main(void) {
     // no error checking
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -55,6 +59,9 @@ int main(void) {
         SCREEN_WIDTH, SCREEN_HEIGHT,
         SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
     ));
+
+    // hide mouse and keep it inside the window
+    SDL_SetRelativeMouseMode(SDL_TRUE);
 
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
@@ -96,15 +103,15 @@ int main(void) {
 
         // left
         {glm::vec3(-0.5f,  0.5f,  0.5f),   glm::vec3(1.0f, 0.0f, 0.0f),  glm::vec2(1.0f, 1.0f)},
-        {glm::vec3(-0.5f,  0.5f, -0.5f),   glm::vec3(0.0f, 1.0f, 0.0f),  glm::vec2(1.0f, 0.0f)},
+        {glm::vec3(-0.5f,  -0.5f, 0.5f),   glm::vec3(0.0f, 1.0f, 0.0f),  glm::vec2(1.0f, 0.0f)},
         {glm::vec3(-0.5f, -0.5f, -0.5f),   glm::vec3(0.0f, 0.0f, 1.0f),  glm::vec2(0.0f, 0.0f)},
-        {glm::vec3(-0.5f, -0.5f,  0.5f),   glm::vec3(1.0f, 0.0f, 1.0f),  glm::vec2(0.0f, 1.0f)},
+        {glm::vec3(-0.5f,  0.5f, -0.5f),   glm::vec3(1.0f, 0.0f, 1.0f),  glm::vec2(0.0f, 1.0f)},
 
         // right
         {glm::vec3(0.5f,  0.5f,  0.5f),   glm::vec3(1.0f, 0.0f, 0.0f),  glm::vec2(1.0f, 1.0f)},
-        {glm::vec3(0.5f, -0.5f,  0.5f),   glm::vec3(1.0f, 0.0f, 1.0f),  glm::vec2(0.0f, 1.0f)},
+        {glm::vec3(0.5f,  0.5f, -0.5f),   glm::vec3(1.0f, 0.0f, 1.0f),  glm::vec2(0.0f, 1.0f)},
         {glm::vec3(0.5f, -0.5f, -0.5f),   glm::vec3(0.0f, 0.0f, 1.0f),  glm::vec2(0.0f, 0.0f)},
-        {glm::vec3(0.5f,  0.5f, -0.5f),   glm::vec3(0.0f, 1.0f, 0.0f),  glm::vec2(1.0f, 0.0f)},
+        {glm::vec3(0.5f, -0.5f,  0.5f),   glm::vec3(0.0f, 1.0f, 0.0f),  glm::vec2(1.0f, 0.0f)},
     }};
 
     std::array<uint32_t, 36> indices;
@@ -212,49 +219,177 @@ int main(void) {
 
     glEnable(GL_DEPTH_TEST);
 
-    int quit = 0;
+    // some globals (outside the game loop)
+    bool quit = false;
+
+    bool up = false;
+    bool down = false;
+    bool left = false;
+    bool right = false;
+
+    float deltaTime = 0.0f; // time between current and last frame
+    float lastFrame = 0.0f; // time of last frame
+
+    // camera
+    glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+    static constexpr float cameraSpeed = 2.5f;
+    float yaw = -90.0f;
+    float pitch = 0.0f;
+    int xoffset = 0;
+    int yoffset = 0;
+    static constexpr float mouseSensitivity = 0.1f;
+    static constexpr float defaultFov = 70.0f;
+    float fov = defaultFov;
+
+    // handle input
     while(!quit) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             switch(event.type) {
                 case SDL_QUIT:
-                    quit = 1;
+                    quit = true;
                     break;
-                case SDL_KEYUP:
-                    if (event.key.keysym.sym == SDLK_ESCAPE) {
-                        quit = 1;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                    case SDLK_w:
+                        up = true;
+                        break;
+                    case SDLK_a:
+                        left = true;
+                        break;
+                    case SDLK_s:
+                        down = true;
+                        break;
+                    case SDLK_d:
+                        right = true;
+                        break;
+                    default:
+                        break;
                     }
                     break;
+                case SDL_KEYUP:
+                    switch (event.key.keysym.sym) {
+                    case SDLK_w:
+                        up = false;
+                        break;
+                    case SDLK_a:
+                        left = false;
+                        break;
+                    case SDLK_s:
+                        down = false;
+                        break;
+                    case SDLK_d:
+                        right = false;
+                        break;
+                    default:
+                        break;
+                    }
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    /* if (event.button.button == SDL_BUTTON_LEFT) { */
+                    /*     mouse_down(LEFT_BUTTON); */
+                    /*     drag(); */
+                    /* } else if (event.button.button == SDL_BUTTON_RIGHT) { */
+                    /*     mouse_down(RIGHT_BUTTON); */
+                    /* } */
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    /* if (event.button.button == SDL_BUTTON_LEFT) { */
+                    /*     mouse_up(LEFT_BUTTON); */
+                    /*     undrag(); */
+                    /* } else if (event.button.button == SDL_BUTTON_RIGHT) { */
+                    /*     mouse_up(RIGHT_BUTTON); */
+                    /* } */
+                    break;
+                case SDL_MOUSEMOTION:
+                    {
+                        // retrieve x and y offset from mouse movement
+                        xoffset = event.motion.xrel;
+                        yoffset = - event.motion.yrel; // down is 1 for SDL but -1 for OpenGL coords
+                        yaw = glm::mod(yaw + xoffset * mouseSensitivity, 360.0f);
+                        pitch += yoffset * mouseSensitivity;
+
+                        // constrain pitch to avoid weird camera movements
+                        if (pitch > 89.0f)
+                            pitch = 89.0f;
+                        if (pitch < -89.0f)
+                            pitch = -89.0f;
+
+                        // calculate camera direction
+                        glm::vec3 direction;
+                        direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+                        direction.y = sin(glm::radians(pitch));
+                        direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+                        cameraFront = glm::normalize(direction);
+                    }
+                    break;
+                case SDL_MOUSEWHEEL:
+                    {
+                        fov -= event.wheel.y;
+
+                        // constain fov
+                        if (fov < 1.0f)
+                            fov = 1.0f;
+                        if (fov > defaultFov)
+                            fov = defaultFov;
+                    }
                 default:
                     break;
             }
         }
 
+        // update
+
+
+        float time = SDL_GetTicks() / 1000.0f;
+        deltaTime = time - lastFrame;
+        lastFrame = time;
+        float cameraOffset = cameraSpeed * deltaTime;
+
+        if (up)
+            cameraPos += cameraOffset * cameraFront;
+        if (down)
+            cameraPos -= cameraOffset * cameraFront;
+        if (left)
+            cameraPos -= cameraOffset * glm::normalize(glm::cross(cameraFront, cameraUp));
+        if (right)
+            cameraPos += cameraOffset * glm::normalize(glm::cross(cameraFront, cameraUp));
 
         // render
         {
             glClearColor(BLACK, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            float time = SDL_GetTicks() / 1000.0f;
             /* float greenValue = sin(time) * 0.5 + 0.5; */
 
             shader.bind();
             /* shader.setUniform4f("uColor", 0, greenValue, 0, 1); */
 
-            // weird rotating plane
 
             // order of transformation is reversed (the last one is the first which is applied)
             glm::mat4 model = glm::mat4(1.0f);
             /* model = glm::translate(model, glm::vec3(0.5f, -0.5f, 0.0f)); */
-            model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); 
-            model = glm::rotate(model, time, glm::vec3(0.0f, 0.0f, -1.0f));
+            /* model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); */ 
+            /* model = glm::rotate(model, time, glm::vec3(1.0f, 1.0f, 1.0f)); */
 
             glm::mat4 view = glm::mat4(1.0f);
             // note that we're translating the scene in the reverse direction of where we want to move
             view = glm::translate(view, glm::vec3(0.0f, 0.0f, -2.0f)); 
             
-            glm::mat4 projection = glm::perspective(glm::radians(70.0f), ASPECT_RATIO, 0.1f, 100.0f);
+            glm::mat4 projection = glm::perspective(glm::radians(fov), ASPECT_RATIO, 0.1f, 100.0f);
+
+            /* glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f); */
+            /* glm::vec3 cameraTarget = glm::vec3(0.0f); */
+            /* glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget); */
+            /* glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f); */
+            /* glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection)); */
+            /* glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight); */
+            view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp); 
 
             // TODO: figure out if better to compute mvp matrix on cpu vs doing it in vertex shader
             shader.SetUniformMatrix4fv("model", model);
